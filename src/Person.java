13c8667578dc069262bf695d9a7bc029acb85dbd
Visal <bestlost3@gmail.com>
2023-05-20 22:41:12 +0530

    Encapsulation

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+class Person {
+    private int age;
+
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setAge(25);
+        System.out.println(p.getAge());
+    }
+}
